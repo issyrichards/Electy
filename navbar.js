d6ce8dfcadcd27c6c0c2a5d1caa7dc99fc9ede1e
Rebecca Marshall <rebeccamarshall@Rebeccas-MacBook-Air.local>
2015-03-12 18:10:39 +0000

    new css

---
+++ navbar.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('.nav');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.nav').css('background-color', 'rgba(34,34,34,0.9)');
+       } else {
+          $('.nav').css('background-color', 'transparent');
+       }
+   });
+});
